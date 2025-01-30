ca897beb4afd5f0a2a21c4bd9ff0202f9b2ed79e
Maniya dhruvi <maniyadhruvi81@gmail.com>
2025-01-30 16:14:18 +0530

    project for udemy

---
+++ src/Componenets/Category-item/Categoryitem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './category-item-style.scss';
+
+const Categoryitem = ({ category }) => {
+    const { imgUrl, title } = category;
+
+    return (
+
+        <div className='category-container'>
+            <div className='background-img' style={{ backgroundImage: `url(${imgUrl})` }} />
+            <div className='category-body-container'>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default Categoryitem
